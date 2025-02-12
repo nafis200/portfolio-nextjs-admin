67fb043c0c75942feebbd6983e8bd057d61cc267
nafis200 <nafisahamed14@gmail.com>
2025-02-12 19:39:51 +0600

    video

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -31,7 +31,7 @@ const LoginPage = () => {
           <div className="flex justify-center gap-4 mt-4">
             <button
               className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
-              onClick={() => signIn("google", { callbackUrl: "http://localhost:3000/dashboard" })}
+              onClick={() => signIn("google", { callbackUrl: "https://portfolio-frontend-gamma-weld.vercel.app//dashboard" })}
             >
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
@@ -42,7 +42,7 @@ const LoginPage = () => {
             </button>
             <button
               className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
-              onClick={() => signIn("github", { callbackUrl: "http://localhost:3000/dashboard" })}
+              onClick={() => signIn("github", { callbackUrl: "https://portfolio-frontend-gamma-weld.vercel.app//dashboard" })}
             >
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
